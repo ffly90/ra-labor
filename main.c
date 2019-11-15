044e27ef9419efddb5727c0f84a4456d8dd5b01f
Steffen Walter <serenity@mtmedia.org>
2019-11-15 10:18:03 +0100

    aufgabe 2.3

--- main.c
+++ main.c
@@ -15,17 +15,18 @@ void setup() {
 	SYSTEM_Initialize();  // set 24 MHz clock for CPU and Peripheral Bus
                           // clock period = 41,667 ns = 0,0417 us
 	TRISDCLR = 1<<pinnumber;     // set bit 3 of Port D for output
+    TRISBSET = 1<<9; // set input RB9 or button s1
 }
 
 void loop() { 
-	int i;
+	//int i;
 	while(1) {
-		LATDSET = 1<<pinnumber;       // set bit 3 of Port D
-		for (i=0; i< 1000000; i++);
-        
-        LATDCLR = 1<<pinnumber;       // clear bit 3 of Port D
-        for (i=0; i< 1000000; i++); 
-  }
+		if(PORTB & 1<<9){
+            LATDCLR = 1<<pinnumber;       // set bit 3 of Port D
+        }else{
+            LATDSET = 1<<pinnumber;       // clear bit 3 of Port D
+        }
+    }
 }
 
 int main(void) {
