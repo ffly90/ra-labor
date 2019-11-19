e59a893db39e053f8c3b475ca55c26355f9ddc18
Steffen Walter <serenity@mtmedia.org>
2019-11-19 11:04:01 +0100

    augabe 5 final

--- main.c
+++ main.c
@@ -23,21 +23,22 @@ void setup() {
     CCP1CON3bits.POLACE = 0;
     CCP1TMRbits.TMRL = 0;
     CCP1PRbits.PRL = 0xffff;
-    CCP1RA = 0x0000 ; // counter overflow value
-    CCP1RB = 0x8000; //
-    CCP1CON1bits.MOD = 0b0101; // center dual edge buffered compare mode
+    CCP1RA = 0 ; // counter overflow value
+    CCP1RB = 0xffff ; //
+    CCP1CON1bits.MOD = 0b0101; // dual edge buffered compare mode
     CCP1CON1bits.CLKSEL = 0; //select sysclk 
-    CCP1CON1bits.TMRPS = 0; // prescaler 1:8
+    CCP1CON1bits.TMRPS = 0; // prescaler 1:1
     CCP1CON1bits.ON = 1; // start timer
 }
 
 void loop() {
-    int i = 0;
-    int b = 0;
+    int k = 0;
 	while(1) {
-        for(i = 0; i<100000; i++){
-            CCP1RA = i;
-            for(b = 0; b<100; b++){}
+        for(CCP1RA = 0; CCP1RA<0xffff; CCP1RA++){
+            for(k = 0; k<100; k++){}
+        }
+        for(CCP1RA = 0xffff; CCP1RA>0 ; CCP1RA--){
+            for(k = 0; k<100; k++){}
         }
     }
 }
