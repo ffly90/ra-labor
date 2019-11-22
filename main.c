9962c487f3349ba12d8717796a0dc06ea5d14c25
Steffen Walter <serenity@mtmedia.org>
2019-11-22 11:36:29 +0100

    termin 2 aufgabe 1

--- main.c
+++ main.c
@@ -9,36 +9,28 @@
 
 void SYSTEM_Initialize(void);
 
-int pinnumber = 0;
+typedef unsigned char u8;
 
 void setup() { 
 	SYSTEM_Initialize();  // set 24 MHz clock for CPU and Peripheral Bus
                           // clock period = 41,667 ns = 0,0417 us
-    CCP1CON1bits.CCSEL = 0;
-    CCP1CON1bits.T32 = 0;
-    CCP1CON1bits.TRIGEN = 0;
-    CCP1CON1bits.SYNC = 0;
-    CCP1CON2bits.OCAEN = 1;
-    CCP1CON3bits.OUTM = 0;
-    CCP1CON3bits.POLACE = 0;
-    CCP1TMRbits.TMRL = 0;
-    CCP1PRbits.PRL = 0xffff;
-    CCP1RA = 0 ; // counter overflow value
-    CCP1RB = 0xffff ; //
-    CCP1CON1bits.MOD = 0b0101; // dual edge buffered compare mode
-    CCP1CON1bits.CLKSEL = 0; //select sysclk 
-    CCP1CON1bits.TMRPS = 0; // prescaler 1:1
-    CCP1CON1bits.ON = 1; // start timer
+    initDisplay(); //initialize disp
+    initSelect(); //init selector
+    T1CONbits.TECS = 0b10;
+    T1CONbits.TCS = 1;
+    T1CONbits.TCKPS = 0b11;
+    T1CONbits.TSYNC = 0;
+    PR1 = 10000;
+    T1CONbits.ON = 1;
 }
 
 void loop() {
-    int k = 0;
-	while(1) {
-        for(CCP1RA = 0; CCP1RA<0xffff; CCP1RA++){
-            for(k = 0; k<100; k++){}
-        }
-        for(CCP1RA = 0xffff; CCP1RA>0 ; CCP1RA--){
-            for(k = 0; k<100; k++){}
+    u8 k, einer, zehner, hunderter;
+    int i;
+    while(1){
+        for(i=0; i<100; i++){
+            writeDec(TMR1/10);
+            //writeDot(1);
         }
     }
 }
