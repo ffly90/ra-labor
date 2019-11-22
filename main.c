267680df04010ef86e93716da8b385f5ecfb09d3
Steffen Walter <serenity@mtmedia.org>
2019-11-22 13:37:52 +0100

    termin 2 aufgabe 3

--- main.c
+++ main.c
@@ -6,9 +6,11 @@
 
 #include <stdint.h>
 #include <xc.h>
+#include <sys/attribs.h>
 
 void SYSTEM_Initialize(void);
 
+int ADCResult;
 typedef unsigned char u8;
 
 void setup() { 
@@ -16,11 +18,13 @@ void setup() {
                           // clock period = 41,667 ns = 0,0417 us
     initDisplay(); //initialize disp
     initSelect(); //init selector
+    //Zähler Konfiguration
+    T1CONbits.TGATE = 0;
     T1CONbits.TECS = 0b10;
     T1CONbits.TCS = 1;
-    T1CONbits.TCKPS = 0b11;
+    T1CONbits.TCKPS = 0;
     T1CONbits.TSYNC = 0;
-    PR1 = 10000;
+    PR1 = 0x7fff;
     T1CONbits.ON = 1;
     //ADC Konfiguration
     ANSELAbits.ANSA0 = 1;
@@ -30,10 +34,15 @@ void setup() {
     AD1CON3bits.ADRC = 0;
     AD1CON3bits.ADCS = 8;
     AD1CON1bits.ON = 1;
+    //Interrupt
+    IEC0bits.T1IE = 1;
+    IFS0bits.T1IF = 0;
+    IPC4bits.T1IP = 2;
+    IPC4bits.T1IS = 0;
 }
 
 unsigned int readADC() {
-    int ADCResult;
+    
     AD1CON1bits.SAMP = 1;
     delay_us(2000);
     AD1CON1bits.SAMP = 0;
@@ -42,12 +51,16 @@ unsigned int readADC() {
     return (ADC1BUF0);
 }
 
+void __ISR(_TIMER_1_VECTOR, IPL2SOFT) handler(void) {
+    ADCResult = readADC();
+    IFS0bits.T1IF = 0;
+}
+
 void loop() {
-    u8 k, einer, zehner, hunderter;
     int i, r;
     while(1){
         for(r=0; r<100; r++){
-            writeDec(readADC());
+            writeDec(ADCResult);
         }
     }
 }
